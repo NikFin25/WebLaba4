e3b41d2ddd9d7db39eabec9caa0f2c028f703e31
NikFin <doublnekit@gmail.com>
2024-10-16 17:09:30 +0300

    commit5

--- orders.js
+++ orders.js
@@ -8,32 +8,14 @@ document.getElementById('orderForm').addEventListener('submit', function(event) 
     orderBlock.innerHTML = `<h3>${customerName}</h3><p>${orderDescription}</p>`;
 
     document.getElementById('blockOrders').appendChild(orderBlock);
-
-    // Сохранение заказа в LocalStorage
-    let orders = JSON.parse(localStorage.getItem('orders')) || [];
-    orders.push({ customerName, orderDescription });
-    localStorage.setItem('orders', JSON.stringify(orders));
-
-    // Очищаем форму после отправки
-    document.getElementById('orderForm').reset();
-
 });
 
-window.onload = function() {
-    let savedOrders = JSON.parse(localStorage.getItem('orders')) || [];
-    savedOrders.forEach(order => {
-        const orderBlock = document.createElement('div');
-        orderBlock.className = 'containersOrder';
-        orderBlock.innerHTML = `<h3>${order.customerName}</h3><p>${order.orderDescription}</p>`;
-        document.getElementById('blockOrders').appendChild(orderBlock);
-    });
-};
 
 window.onload = function() {
-    const formBlock = document.getElementById('formBlock');
+    const formBlock = document.getElementById('mainBlock');
     formBlock.style.visibility = 'visible';
     formBlock.animate([
-        {left: '-100%'},
+        {left: '-50%'},
         {left: '0%'}
     ], {
         duration: 1000,
